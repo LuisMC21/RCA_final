7f7a940b41907ce6e98a761f3201e7ca32630264
IsaacRS <crs170919@gmail.com>
2022-10-12 18:34:30 -0500

    first commit

---
+++ src/main/java/com/rca/RCA/RcaApplication.java
@@ -0,0 +1,13 @@
+package com.rca.RCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rca/RCA/RcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rca.RCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
