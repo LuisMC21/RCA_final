86905e323ced14b9cace2725a202de1757f0e756
IsaacCRS17 <crs170919@gmail.com>
2022-10-15 01:00:45 -0500

    united codes

--- src/main/java/com/rca/RCA/util/Code.java
+++ src/main/java/com/rca/RCA/util/Code.java
@@ -7,6 +7,26 @@ public class Code {
     public static final String GRADE_CODE = "GR";
     public static final int GRADE_LENGTH = 5;
 
+    public static final String ROL_CODE = "ROL";
+    public static final int ROL_LENGTH = 6;
+
+    public static final String USUARIO_CODE = "USU";
+    public static final int USUARIO_LENGTH = 6;
+
+    public static final String IMAGEN_CODE = "IMG";
+    public static final int IMAGEN_LENGTH = 6;
+
+    public static final String NEWS_CODE = "NEWS";
+    public static final int NEWS_LENGTH = 6;
+
+    public static final String APO_CODE = "APO";
+    public static final int APO_LENGTH = 6;
+
+    public static final String ALU_CODE = "ALU";
+    public static final int ALU_LENGTH = 6;
+
+    public static final String ASIS_CODE = "ASIS";
+    public static final int ASIS_LENGTH = 6;
     public static final String SECTION_CODE = "SEC";
     public static final int SECTION_LENGTH = 6;
 
