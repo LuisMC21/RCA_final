32594e38a6fadc792545a1d52588d9a742c7f5cb
IsaacCRS17 <crs170919@gmail.com>
2023-07-17 22:14:13 -0500

    "update"

--- src/main/java/com/rca/RCA/type/AlumnoDTO.java
+++ src/main/java/com/rca/RCA/type/AlumnoDTO.java
@@ -1,16 +1,24 @@
 package com.rca.RCA.type;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AlumnoDTO extends AuditoryDTO{
     private String code;
+    @NotBlank
     private String diseases;
+    @NotBlank
     private String namecon_pri;
+    @NotBlank
     private String telcon_pri;
+    @NotBlank
     private String namecon_sec;
+    @NotBlank
     private String telcon_sec;
+    @NotBlank
     private String vaccine;
+    @NotBlank
     private String type_insurance;
     private ApoderadoDTO apoderadoDTO;
     private UsuarioDTO usuarioDTO;
